b48951c295a12adc8a6afeb988219292b3619f1a
Owen6693 <1009697882@qq.com>
2025-03-29 23:05:10 +0800

    Delete State-Archives-Transcription directory

--- State-Archives-Transcription/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app','static','uploads')
-    # Allowed file extensions for image/PDF uploads
-    ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-    # Add additional configuration settings, e.g., SQLALCHEMY_DATABASE_URI for database connections
