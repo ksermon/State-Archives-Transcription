7a6fee9ec17c69331ede5cebd467d534dcb93f09
Owen6693 <1009697882@qq.com>
2025-03-29 23:15:19 +0800

    Delete State-Archives-Transcription directory

--- State-Archives-Transcription/install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-where pip >nul 2>&1 || (echo pip is required & exit /B 1)
-where virtualenv >nul 2>&1 || pip install virtualenv
-
-virtualenv venv
-call venv\Scripts\activate
-pip install -r requirements.txt
-
-flask run
