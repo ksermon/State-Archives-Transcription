7c47bc14425cd95d8e6bd90870d1b6a3fe035082
Ben Daniel <lockypood@gmail.com>
2025-05-22 13:45:43 +0800

    Easier implementation into main, will not break existing model

--- app/utils/ocr_engine/__init__.py
+++ app/utils/ocr_engine/__init__.py
@@ -6,20 +6,23 @@ from PIL import Image
 import io
 import base64
 
+from .ocr_model import load_model, recognize_single_image, recognize_batch_images
+from .slicer import auto_slice_lines
+from .preprocessing import preprocess_image
+from .aggregator import aggregate_text
+from PIL import Image
+
+# def run_ocr_engine(image_path, processor, model):
 def run_ocr_engine(image_base64):
     image = Image.open(io.BytesIO(base64.b64decode(image_base64)))
+    # image = Image.open(image_path)
+    
     # preprocessing
     preprocessed = preprocess_image(image)
     line_imgs = auto_slice_lines(preprocessed)
-    lines = recognize_batch_images(line_imgs, processor, model)
     
-    # OCR per line using preloaded model
-    # lines = []
-    # for line_img in line_imgs:
-    #     text = recognize_single_image(line_img)
-    #     lines.append(text)
-    
-    # lines = [recognize_single_image(line_img, processor, model) for line_img in line_imgs]
+    # lines = recognize_batch_images(line_imgs, processor, model)
+    lines = [recognize_single_image(line_img) for line_img in line_imgs]
     
     final_text = aggregate_text(lines)
     return final_text
