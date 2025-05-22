9467fc7736c10fa4e06181b844dd8818b1ac05c8
byteDJINN <developerdjay@gmail.com>
2025-05-22 05:54:09 +0000

    Fix: Pass processor and model to recognize_single_image in run_ocr_engine

--- app/utils/ocr_engine/__init__.py
+++ app/utils/ocr_engine/__init__.py
@@ -10,13 +10,14 @@ import base64
 def run_ocr_engine(image_base64):
     image = Image.open(io.BytesIO(base64.b64decode(image_base64)))
     # image = Image.open(image_path)
+    processor, model = load_model()
     
     # preprocessing
     preprocessed = preprocess_image(image)
     line_imgs = auto_slice_lines(preprocessed)
     
     # lines = recognize_batch_images(line_imgs, processor, model)
-    lines = [recognize_single_image(line_img) for line_img in line_imgs]
+    lines = [recognize_single_image(line_img, processor, model) for line_img in line_imgs]
     
     final_text = aggregate_text(lines)
     return final_text
